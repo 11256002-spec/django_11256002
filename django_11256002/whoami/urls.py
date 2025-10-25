a9f8277390220a842e054e3ca1884f3c7f8b1348
張愷庭 <11256002@ntub.edu.tw>
2025-10-25 10:10:52 +0800

    更新程式碼

--- django_11256002/whoami/urls.py
+++ django_11256002/whoami/urls.py
@@ -3,4 +3,5 @@ from .views import whoami_view
 
 urlpatterns = [
     path('whoamI', whoami_view),
+    path('', whoami_view),
 ]
--- django_11256002/whoami/views.py
+++ django_11256002/whoami/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from whoami.views import whoami_view
 
 def whoami_view(request):
     return render(request, 'whoami.html')
